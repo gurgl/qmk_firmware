81034542bbb7ae256212d36d26e2b3a0cb423e00
Karl Widell <karl.widell@ucr.uu.se>
2019-03-27 17:31:34 +0100

    reverted some changes that made it all very confusing

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/gurgl/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/gurgl/keymap.c
@@ -45,10 +45,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_LBRC,
      KC_LSFT, KC_A  , KC_S  , KC_D  , MT(MOD_LSFT,KC_F)  , KC_G  ,            KC_H  , MT(MOD_LSFT,KC_J)  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
      KC_LCTL, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_UNDS,
-                      TG(_RAISE)  ,KC_TAB,                                                       KC_BSPC,TG(_LOWER),
-                                      KC_SFTENT,KC_SPC,                           KC_BSPC, LOWER,
-                                      RAISE,KC_LCTL,                         MT(MOD_LALT,KC_TAB), KC_RCTL,
-                                      KC_LSFT, KC_TAB,                       KC_LGUI, MT(MOD_LALT,KC_APP)
+                      TG(_RAISE)  ,_______,                                                       KC_DEL,TG(_LOWER),
+                                    LT(RAISE,KC_TAB),KC_SPC,                           KC_SFTENT, LT(LOWER,KC_BSPC),
+                                      KC_LSFT,KC_LCTL,                        MT(MOD_LALT,KC_APP), KC_RCTL,
+                                      KC_TAB, KC_LALT,                       KC_LGUI, MT(MOD_LALT,KC_APP)
   ),
 
   [_LOWER] = LAYOUT_5x6(
