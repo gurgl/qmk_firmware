008a115fc8b82141661df7a6e41b88cd1c882063
Karl Widell <karl.widell@ucr.uu.se>
2019-03-27 04:50:52 +0100

    stuff

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/gurgl/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/gurgl/keymap.c
@@ -35,19 +35,20 @@ PROBLEMS
 * a bit disturbing to have important keys like bkspace and enter activate on release
 
 */
-
+//LT(LOWER,KC_BSPC)
+//  LT(RAISE,KC_TAB)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT_5x6(
      KC_ESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_BSLASH,
      KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_LBRC,
-     KC_LSFT, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
+     KC_LSFT, KC_A  , KC_S  , KC_D  , MT(MOD_LSFT,KC_F)  , KC_G  ,            KC_H  , MT(MOD_LSFT,KC_J)  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
      KC_LCTL, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_UNDS,
-                      TG(_RAISE)  ,_______,                                                       KC_DEL,TG(_LOWER),
-                                    LT(RAISE,KC_TAB),KC_SPC,                           KC_SFTENT, LT(LOWER,KC_BSPC),
-                                      KC_LSFT,KC_LCTL,                        MT(MOD_LALT,KC_APP), KC_RCTL,
-                                      KC_TAB, KC_LALT,                       KC_LGUI, MT(MOD_LALT,KC_APP)
+                      TG(_RAISE)  ,KC_TAB,                                                       KC_BSPC,TG(_LOWER),
+                                      KC_SFTENT,KC_SPC,                           KC_BSPC, LOWER,
+                                      RAISE,KC_LCTL,                         MT(MOD_LALT,KC_TAB), KC_RCTL,
+                                      KC_LSFT, KC_TAB,                       KC_LGUI, MT(MOD_LALT,KC_APP)
   ),
 
   [_LOWER] = LAYOUT_5x6(
@@ -65,11 +66,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_RAISE] = LAYOUT_5x6(
     KC_F12,  KC_F1 , KC_F2 , KC_F3 ,KC_F4,  KC_F5 ,                           KC_F6,      KC_F7,     KC_F8,      KC_F9,     KC_F10,          KC_F11 ,
-    KC_PAST,KC_PSLS,KC_P7 , KC_P8 , KC_P9 ,KC_NLCK,                           OSL(_TOP),  KC_NUBS,   LSFT(KC_NUBS),LSFT(KC_EQL), KC_EQL,       KC_PLUS,
-    KC_PPLS,KC_PMNS,KC_PIPE ,RALT(KC_7),RALT(KC_0),   KC_NUHS,                KC_SE_TILD, LSFT(KC_8),LSFT(KC_9),   RALT(KC_8),RALT(KC_9),    KC_LPRN,
-    KC_UNDS,_______,KC_LALT,  KC_LSFT,KC_LCTL,NO_CIRC,                          KC_LGUI,     KC_LCTL,       KC_LSFT,      KC_LALT,   RALT(KC_NUBS), KC_GRV,
-                          _______,KC_P0  ,                                          OSM(MOD_MEH),_______,
-                                                _______,_______,              _______,_______,
+    KC_PAST,KC_PSLS,KC_P7 , KC_P8 , KC_P9 ,KC_NLCK,                           OSL(_TOP),  KC_NUBS,   LSFT(KC_NUBS),RALT(KC_7),RALT(KC_0),       NO_CIRC,
+    KC_PPLS,KC_PMNS,KC_PIPE ,LSFT(KC_EQL), KC_EQL,   KC_NUHS,                KC_SE_TILD, LSFT(KC_8),LSFT(KC_9),   RALT(KC_8),RALT(KC_9),    KC_LPRN,
+    KC_UNDS,_______,KC_LALT,  KC_LSFT,KC_LCTL,KC_PLUS,                          KC_LGUI,     KC_LCTL,       KC_LSFT,      KC_LALT,   RALT(KC_NUBS), KC_GRV,
+                          _______,KC_P0  ,                                          KC_DEL,_______,
+                                                _______,_______,              _______,KC_BSPC,
                                                 _______,_______,              _______,_______,
                                                 _______,_______,              _______,_______
   ),
